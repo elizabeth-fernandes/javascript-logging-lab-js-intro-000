45ac13ae2de83cb3c087033a862828f622f30bfa
Elizabeth Fernandes <emf34765@usc.edu>
2020-12-13 04:53:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-console.error("HALP!")
-console.log("Continue")
-console.warn("Warning")
+// declare the variable
+var word
+
+// assign a value to the variable
+word = 'bird'
+
+console.log(word) // 'bird'
+
+// assign another value to the variable
+word = 'dog'
+
+console.log(word) // 'dog'
+Learn
